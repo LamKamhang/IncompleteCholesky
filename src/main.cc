5b35a0e3cd9bfa78ad348db4fef1bdcd0bb227f2
LamKamhang <Cool_Lam@outlook.com>
2019-12-03 12:11:03 +0800

    add another test

--- src/main.cc
+++ src/main.cc
@@ -16,74 +16,28 @@ int main(int argc, char *argv[])
 {
   auto seed = 1;
 
-  int n = 3000;
-  int nnz = 5000;
+  // int n = 3000;
+  // std::vector<int> nnzs = {
+  //   5000, 10000, 15000, 20000, 25000, 30000, 90000, 900000, 4000000, 9000000
+  // };
+  // for (auto & nnz : nnzs )
+  // {
+  //   EvaluateSolver<SimplicialCholesky<SparseMatrix<double> > >("Sparse Cholesky solver", seed, nnzSparse<>, n, nnz);
+  //   // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
+  //   // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
+  //   EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
+  //   EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
+  // }
 
-  // EvaluateSolver<SimplicialCholesky<SparseMatrix<double> > >("Sparse Cholesky Solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialCholesky<SparseMatrix<double> > >("Sparse Cholesky Solver", seed, random_SPD<>, 20);
-
-  EvaluateSolver<SimplicialCholesky<SparseMatrix<double> > >("Sparse Cholesky solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-
-  nnz = 10000;
-  EvaluateSolver<SimplicialCholesky<SparseMatrix<double> > >("Sparse Cholesky solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-
-  nnz = 15000;
-  EvaluateSolver<SimplicialCholesky<SparseMatrix<double> > >("Sparse Cholesky solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-
-  nnz = 20000;
-  EvaluateSolver<SimplicialCholesky<SparseMatrix<double> > >("Sparse Cholesky solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-
-  nnz = 25000;
-  EvaluateSolver<SimplicialCholesky<SparseMatrix<double> > >("Sparse Cholesky solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-
-  nnz = 30000;
-  // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-
-  nnz = 90000;
-  // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-
-  nnz = 900000;
-  // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-
-  nnz = 4000000;
-  // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-
-  nnz = 9000000;
-  // EvaluateSolver<SimplicialLLT<SparseMatrix<double> > >("Sparse Cholesky LLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<SimplicialLDLT<SparseMatrix<double> > >("Sparse Cholesky LDLT solver", seed, nnzSparse<>, n, nnz);
-  // EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
-  EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, nnz);
+  int dense = 2;
+  std::vector<int> ns = {
+    500, 1000, 5000, 10000, 200000
+  };
+  for (auto & n : ns)
+  {
+    EvaluateSolver<SimplicialCholesky<SparseMatrix<double> > >("Sparse Cholesky solver", seed, nnzSparse<>, n, dense*n);
+    EvaluateSolver<IncompleteCholesky<double> >("Incomplete Cholesky solver", seed, nnzSparse<>, n, dense*n);
+    EvaluateSolver<CudaIncompleteCholesky<double> >("cuda Incomplete Cholesky solver", seed, nnzSparse<>, n, dense*n);
+  }
   return 0;
 }
